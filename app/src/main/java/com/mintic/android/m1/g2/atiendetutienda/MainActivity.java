c1de3845ac962d3ebde73823c93182e56522902b
miguelta281@gmail.com <miguelta281@gmail.com>
2021-07-07 21:53:09 -0500

    Crear el proyecto

---
+++ app/src/main/java/com/mintic/android/m1/g2/atiendetutienda/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mintic.android.m1.g2.atiendetutienda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
